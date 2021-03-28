3f3afe64bfa39bc31f1e7f06be8af6ca6db887f8
Thomas Tresgots <ttresgots@gmail.com>
2021-03-29 00:13:34 +0200

    c pa bo

--- project/views.py
+++ project/views.py
@@ -1,6 +1,32 @@
 from . import app
 from flask import render_template
+import matplotlib.pyplot as plt
 
-@app.route('/')
-def index():
+def test():
+    data = [1,2,3,2,5]
+    data2 = [2,4,3,6,6]
+    return print(type(plt.plot(data,data2)))
+
+
+
+
+
+
+def test2():
     return render_template("index.html")
+
+
+@app.route('/test')
+def index():
+    return test()
+
+
+
+
+@app.route('/test2')
+def index2():
+    return test2()
+
+
+
+
