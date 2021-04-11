ce052fbeb6e43ee022740adbdd010486e5631ef3
reda <reda.maizate@gmail.com>
2021-04-11 15:23:03 +0200

    Working with mongo and elasticsearch

--- project/views.py
+++ project/views.py
@@ -1,10 +1,8 @@
 from . import app
 from .python.homepage import homePage
 from .python.ajax.test import *
-from .python.mongo.mongo import *
-from flask import render_template
-
-
+from .python.mongoosastic.mongoosastic import *
+from flask import render_template, jsonify
 
 @app.route('/test')
 def test():
@@ -26,3 +24,28 @@ def hello_word():
 def mongotest():
     res = get()
     return render_template("testmongo.html", res=res)
+
+#### First version is working with Mongo
+
+# @app.route("/index", methods=["GET"])
+# def search_results():
+#     query = request.args.get('q')
+#     projects = db.collections.find({"product_name": query}).limit(5)
+#     print(projects)
+#     return render_template("index2.twig", projects=projects)
+
+#### Second version working with Elasticsearch
+
+@app.route("/index", methods=["GET"])
+# Accés par la page /index?q
+def search_results():
+    query = request.args.get('q')
+    a = es1.search(index="test", body={
+                                       "from": 0, "size": 5,
+                                        "query": {
+                                            "match":
+                                                {"product_name": query,
+                                                 }
+                                        }
+    })
+    return render_template("index2.twig", projects=a)
