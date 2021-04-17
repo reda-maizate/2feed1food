9ec711633d7474e4304fe7c80f73d2eaa4163681
reda-maizate <reda-maizate@estiam.com>
2021-04-17 23:36:15 +0200

    Test

--- notebook/init.py
+++ notebook/init.py
@@ -16,40 +16,19 @@ warnings.filterwarnings("ignore")
 print("LOG: Adding data on the Mongo Database!")
 
 if "-light" in opts:
-    with open('mongoDB/2feed1food-data.json', 'r') as myfile:
+    with open('notebook/mongoDB/2feed1food-light.json', 'r') as myfile:
         file=myfile.read()
 
     data = json.loads(file)
     db.collections.insert_many(data)
     print("LOG: Installation of the light version done!")
 elif "-full" in opts:
-    # Not the best solution, reflection on going.. 
-    print("LOG: Starting download of the data! (The download can takes more than 10mn)")
-    openfoodfacts.utils.download_data() # This file is X.XXGb.
-    print("LOG: Data downloaded!")
+    with open('notebook/mongoDB/2feed1food-full.json', 'r') as myfile:
+        file=myfile.read()
 
-    print("LOG: Moving the data to the mongo directory!")
-    shutil.move("openfoodfacts-mongodbdump.tar.gz", "../data/mongo/openfoodfacts-mongodbdump.tar.gz")
-    print("LOG: Succesfully moved the data to the mongo directory!")
-
-    os.chdir("../data/mongo")
-    print("LOG: Changed directory, now in /data/mongo/")
-
-    tar = tarfile.open("openfoodfacts-mongodbdump.tar.gz", "r:gz")
-    print("LOG: Extracting starting! (The extraction can takes more than 10mn)")
-    tar.extractall()
-    print("LOG: Extracting finished!")
-    tar.close()
-
-    os.remove("openfoodfacts-mongodbdump.tar.gz")
-    print("LOG: openfoodfacts-mongodbdump.tar.gz deleted!")
-    print("-----------------------------")
-    print("# MANUAL COMMAND TO EXECUTE #")
-    print("To finish the installation:")
-    print("- Open a new command line and run 'docker exec -it mongo bash'")
-    print("- To restore the full database, run 'mongorestore /data/db/dump'")
-    print("- Wait until the end of the execution.")
-    print("----------------------------")
+    data = json.loads(file)
+    db.collections.insert_many(data)
+    print("LOG: Installation of the full version done!")
 else:
     raise SystemExit(f"Usage: {sys.argv[0]} (-light | -full)")
 
