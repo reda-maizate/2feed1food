b0b5464ff7ecba61f7117d5a1a5006dbc4d6917f
Thomas Tresgots <ttresgots@gmail.com>
2021-04-27 13:54:02 +0200

    split code, new route /score

---
+++ project/python/ajax/group_by.py
@@ -0,0 +1,25 @@
+import numpy
+from flask import render_template
+
+from notebook.mongoosastic.mongoosastic import *
+
+
+def get_by_score(grade, _from=0, _size=10):
+    lines = collections.find({"nutrition_grade_fr": grade}).sort([("nutrition_grade_fr", -1)]).skip(_from).limit(_size)
+    return render_template('blocks/poster.twig', header=grade, cursor=lines)
+
+
+def group_by():
+    # collections.create_index([("nutrition_grade_fr", 1)])
+    tableContent = ""
+    vals = collections.aggregate([
+        {"$match": {"nutrition_grade_fr": {"$ne": numpy.nan}}},
+        {"$group": {"_id": "$nutrition_grade_fr"}},
+    ])
+    vals = list(vals)  # if this line doesn't exist, vals cursor is not iterable anymore after render_template ://////
+
+    tableHeader = render_template('blocks/poster_header.twig', vals=vals)
+    for val in vals:
+        tableContent += get_by_score(val['_id'])
+
+    return render_template('byScore.twig', tableHeader=tableHeader, tableContent=tableContent)
--- project/views.py
+++ project/views.py
@@ -1,10 +1,10 @@
 from flask import request
 
-from notebook.mongoosastic.mongoosastic import *
 from . import app
 from .python.GET.get_product import *
 from .python.ajax.searchBar import *
 from .python.ajax.search_specs import *
+from .python.ajax.group_by import *
 
 
 @app.route('/')
@@ -37,40 +37,6 @@ def get_product_route():
     return get_product(request.args.get('doc'))
 
 
-@app.route("/max")
-def get_max_route():
-    # collections.create_index([("nutrition_grade_fr", 1)])
-    html = ""
-    vals = collections.aggregate([
-        {"$match": {"nutrition_grade_fr": {"$ne": numpy.nan}}},
-        {"$group": {"_id": "$nutrition_grade_fr"}},
-    ])
-
-    for val in vals:
-        lines = collections.find({"nutrition_grade_fr": val['_id']}).sort([("nutrition_grade_fr", -1)]).limit(5)
-        # return str(header)
-        html += render_template('blocks/poster.twig', header=val, cursor=lines)
-
-    return render_template('byScore.twig', html=html)
-    # return render_template('blocks/poster.twig', cursor=col)
-
-    # # for doc in col:
-    # #     return json.dumps(doc)
-    #
-    # index = es1.search(index="off_collections", body={
-    #     "from": 300, "size": 10,
-    #     "sort": [
-    #         {
-    #             "nutrition_grade_fr": "asc"
-    #                 # {
-    #                 #     "order": "asc",
-    #                 # }
-    #         }
-    #         # {
-    #         #     "nutrition-score-fr_100g": "desc",
-    #         # },
-    #         # {
-    #         #     "nutrition-score-uk_100g": "desc",
-    #         # }
-    #     ]
-    # })
+@app.route("/score")
+def get_score_route():
+    return group_by()
