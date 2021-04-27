97807edd7353e974e675df66320b4a2059de039b
Thomas Tresgots <ttresgots@gmail.com>
2021-04-27 03:03:37 +0200

    new route /max, displays 5products/categorie;  remove cdn link for bootstrap 4

--- project/views.py
+++ project/views.py
@@ -1,10 +1,10 @@
-from . import app
 from flask import request
 
 from notebook.mongoosastic.mongoosastic import *
+from . import app
+from .python.GET.get_product import *
 from .python.ajax.searchBar import *
 from .python.ajax.search_specs import *
-from .python.GET.get_product import *
 
 
 @app.route('/')
@@ -36,25 +36,41 @@ def ai_route():
 def get_product_route():
     return get_product(request.args.get('doc'))
 
+
 @app.route("/max")
 def get_max_route():
     # collections.create_index([("nutrition_grade_fr", 1)])
-    col = collections.find().sort([("nutrition_grade_fr", -1)])
+    html = ""
+    vals = collections.aggregate([
+        {"$match": {"nutrition_grade_fr": {"$ne": numpy.nan}}},
+        {"$group": {"_id": "$nutrition_grade_fr"}},
+    ])
 
-    for doc in col:
-        return json.dumps(doc)
+    for val in vals:
+        lines = collections.find({"nutrition_grade_fr": val['_id']}).sort([("nutrition_grade_fr", -1)]).limit(5)
+        # return str(header)
+        html += render_template('blocks/poster.twig', header=val, cursor=lines)
 
-# index="off_collections", body={
-#         "from": 300, "size": 10,
-#         "sort": [
-#             {
-#                 "nutrition_grade_fr": "desc",
-#             },
-#             {
-#                 "nutrition-score-fr_100g": "desc",
-#             },
-#             {
-#                 "nutrition-score-uk_100g": "desc",
-#             }
-#         ]
-#     })
+    return render_template('byScore.twig', html=html)
+    # return render_template('blocks/poster.twig', cursor=col)
+
+    # # for doc in col:
+    # #     return json.dumps(doc)
+    #
+    # index = es1.search(index="off_collections", body={
+    #     "from": 300, "size": 10,
+    #     "sort": [
+    #         {
+    #             "nutrition_grade_fr": "asc"
+    #                 # {
+    #                 #     "order": "asc",
+    #                 # }
+    #         }
+    #         # {
+    #         #     "nutrition-score-fr_100g": "desc",
+    #         # },
+    #         # {
+    #         #     "nutrition-score-uk_100g": "desc",
+    #         # }
+    #     ]
+    # })
