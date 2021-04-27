8d8311f94b9a44436311f312325f09d2ba7a458a
Thomas Tresgots <ttresgots@gmail.com>
2021-04-27 14:56:11 +0200

    add right links, remove comment block

--- project/python/ajax/searchBar.py
+++ project/python/ajax/searchBar.py
@@ -47,28 +47,3 @@ def get_item(request):
         'message': 'No results found'
     }
 
-# def get_item(param):
-#     data_cols = ['fat_100g', 'carbohydrates_100g', 'sugars_100g', 'fiber_100g', 'proteins_100g', 'salt_100g',
-#                  'nutrition-score-fr_100g', 'nutrition-score-uk_100g']
-#     res = es1.search(index="off_collections", body={
-#         "from": 0, "size": 5,
-#         "query": {
-#             "query_string": {
-#                 "fields": ["product_name"],
-#                 "query": f"*{param}*",
-#             },
-#         },
-#         "fields": data_cols,
-#     })
-#     # row = res["hits"]["hits"][0]['_source']
-#
-#     returned = {'labels': (data_cols,), 'data': []}
-#
-#     for row in res["hits"]["hits"]:
-#         returned['data'].append({
-#             'data': [row['_source'][key] for key in data_cols],
-#             'label': row['_source']['product_name'],
-#             'backgroundColor': random_color(),
-#         })
-#
-#     return returned
